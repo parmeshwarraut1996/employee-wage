c0c85576202fc8d5e27d2abdd02a6830b82642d8
parmeshwarraut1996 <parmeshwarraut1996@gmail.com>
2020-01-10 11:55:49 +0530

    [Parmeshwar]Add. replace user file

---
+++ basicPrograms/replaceUserName.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x 
+
+#Problem-statement:User Input and Replace String Template “Hello <<UserName>>, How are you?” 
+#Description:Take UserName as Input. Ensure UserName has min 3 char. Replace <<UserName>> with the proper name.
+#File:replaceUserName.sh
+
+str="Hello <<UserName>>, How are you?"
+echo Original String = $str
+
+read -p "Enter string to replace <<userName>> = " replaceUserName
+	if [ $((${#replaceUserName})) -ge 3 ]
+	then
+		echo Replace string = "${str/<<UserName>>/$replaceUserName}"
+	
+	fi
+
