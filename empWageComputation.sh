282c8014b6f7095dd7e386ea2187710a51ebc2b1
parmeshwarraut1996 <parmeshwarraut1996@gmail.com>
2020-01-13 19:21:02 +0530

    [Parmeshwar]Added. wage of part time employee

---
+++ empWageComputation.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+
+
+present=1;
+absent=0;
+wage_per_hour=20;
+working_hours=8;
+wage_per_day=0;
+wage_of_part_time_employee=0;
+wage_per_hour_of_part_time=10;
+echo Welcome to Employee wage Computation
+echo 1)Permanent Employee
+echo 2)Part Time Employee
+echo Enter choice=
+read choice; 
+random_value=$RANDOM 
+	if [ $random_value -ge $present ]
+	then
+		while:
+		do
+			case $choice in 
+				1)
+					echo -------Permanent Employee wage Computation------- 
+					echo Employee present
+					wage_per_day=$(( wage_per_hour*working_hours ))
+					echo wage_per_day=$wage_per_day
+					break
+				2)
+					echo --------Part Time Employee--------
+					wage_of_part_time_employee=$(( wage_per_hour_of_part_time*working_hours ));
+					echo Wage of part_time employee=$wage_of_part_time_employee
+					break
+				*)
+					echo Invalid choice
+			esac
+		done
+
+	else
+	echo Employee absent
+	fi
+
+_
+
