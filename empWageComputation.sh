9eca7124cc2853e7c07cdf736b506c5a1b853076
parmeshwarraut1996 <parmeshwarraut1996@gmail.com>
2020-01-16 00:33:24 +0530

    [Parmeshwar]Refactor. business logic of total wage computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,7 +10,8 @@ working_days=20;
 total_working_hours=0;
 total_working_days=0;
 total_wage_of_Employee=0;
-
+present_working_days=0;
+absent_days=0;
 getWorkingHours(){
 	working_hours=$1;
 }
@@ -24,34 +25,36 @@ read choice
 until (( $total_working_days == 20   && $total_working_hours >= 100 ))
 do
  
-	random_value=`awk -v seed="$RANDOM" 'BEGIN { srand(seed);  printf("%.1f\n", rand()) }'` 
+	random_value=`awk -v seed="$RANDOM" 'BEGIN { srand(seed);  printf("%.1f\n", rand()) }'`
 	random_value=$( printf "%.0f" $random_value )
+        echo random_value=$random_value
 
 		if [ $random_value -ge $present ]
 		then
 			case $choice in 
 			1)
-				echo -------Permanent Employee wage Computation------- 
+
+                                echo -------Permanent Employee wage Computation------- 
 				echo Employee present
-				echo -------Total wage  -------
 				total_wage_of_Employee=$((wage_per_hour * working_hours))
 				((wage_per_day+=total_wage_of_Employee))
-				echo Total wage=$wage_per_day
+				((present_working_days+=1))
+				echo working_day=$total_working_days
 				echo -------Wage per day-------
 				echo Wage per day =$((wage_per_hour * working_hours))
 				echo ------Wage per month-------
 				wage_per_month=$(( working_hours*working_days*wage_per_hour ))
 				echo Wage per month =$wage_per_month;
+				echo 
 				((total_working_hours=total_working_hours + working_hours))
-				((total_working_days=total_working_days + 1))
 				;;
 			2)
 				echo --------Part Time Employee--------
 				echo Employee present
-				echo -------Total wage--------
 				total_wage_of_Employee=$((wage_per_hour_of_part_time*working_hours))
 				((wage_per_day+=total_wage_of_Employee))
-				echo wage per day=$wage_per_day 
+				((present_working_days+=1))
+				echo working_days=$total_working_days
 				echo -------Wage per day-------
 				wage_of_part_time_employee=$(( wage_per_hour_of_part_time*working_hours ));
 				echo Wage of part_time employee=$wage_of_part_time_employee
@@ -59,8 +62,7 @@ do
 				wage_per_month=$(( working_hours*working_days*wage_per_hour_of_part_time ))
 				echo Wage per month=$wage_per_month;
          			((total_working_hours=total_working_hours + working_hours))
-                                ((total_working_days=total_working_days + 1))
-				;;
+                                ;;
 			*)
 				echo Invalid choice
 				;;
@@ -68,7 +70,15 @@ do
 
 
 		else
+		#echo Employee absent
+		((total_working_hours=total_working_hours + working_hours))
+                ((absent_days+=1))
+		echo working_day=$total_working_days
 		echo Employee absent
 		fi
+		((total_working_days+=1))
 done
 
+echo total_present=$present_working_days;
+echo total_absent=$absent_days;
+echo total_wage=$wage_per_day;
